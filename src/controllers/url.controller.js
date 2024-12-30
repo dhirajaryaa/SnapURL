a3839b3f571418f394b16191f3e1af283a1aeaf9
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-12-30 21:09:48 +0530

    feat: implement URL shortening functionality using nanoid

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -1,3 +1,26 @@
-export const handleGenerateUrlId = async(req,res)=>{
-    
-}
+import { nanoid } from "nanoid";
+import {Url} from "../models/url.model.js"
+
+export const handleGenerateUrlId = async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+   return res.status(400).json({
+      error: "Url is required!",
+    });
+
+    const shortId = nanoid(8);
+
+   await Url.create({
+        shortId:shortId,
+        redirect_url:url,
+        visit_history:[]
+    });
+
+    return res.status(201).json({
+        shortid:shortId,
+        url:`${process.env.BASE_URL}/${shortId}`
+    })
+  };
+
+};
