c7e4584353c93043b958214febef8e1b20370ff1
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-01-10 14:28:19 +0530

    feat: add statistics endpoint for short URLs to retrieve visit count and history

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -44,4 +44,27 @@ const handleRedirectUrl = async (req, res) => {
   return res.redirect(redirect_data.redirect_url);
 };
 
-export { handleGenerateUrlId,handleRedirectUrl };
+const handleStats = async (req,res)=>{
+  const {shortId} = req.params ;
+  if(!shortId){
+    res.status(400).json({
+      error: "ShortId is required!",
+    });
+  };
+
+  const redirect_data = await Url.findOne({shortId:shortId});
+  if (!redirect_data) {
+    return res.status(404).json({
+      error: "ShortId not found!",
+    });
+  }
+  return res.status(200).json({
+    shortId:redirect_data.shortId,
+    redirect_url:redirect_data.redirect_url,
+    visit_count:redirect_data.visit_history.length,
+    visit_history:redirect_data.visit_history
+  });
+  
+};
+
+export { handleGenerateUrlId,handleRedirectUrl,handleStats };
--- src/routes/url.route.js
+++ src/routes/url.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { handleGenerateUrlId, handleRedirectUrl } from "../controllers/url.controller.js";
+import { handleGenerateUrlId, handleRedirectUrl, handleStats } from "../controllers/url.controller.js";
 
 export const urlRouter = Router();
 
 urlRouter.route("/url").post(handleGenerateUrlId);
 
 urlRouter.route("/:shortId").get(handleRedirectUrl);
+
+urlRouter.route("/stats/:shortId").get(handleStats);
